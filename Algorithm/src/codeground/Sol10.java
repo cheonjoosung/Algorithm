ebfc725c6ec97636e3a0a34f19924ceeb83506fe
cjswntjd <cheonjoosung@cheonjuseong-ui-MacBook-Pro.local>
2017-08-17 17:48:46 +0900

    add some changes

--- Algorithm/src/codeground/Sol10.java
+++ Algorithm/src/codeground/Sol10.java
@@ -26,68 +26,67 @@ public class Sol10 {
 			int col = sc.nextInt();
 			int block = sc.nextInt();
 
-			long totalCount = ( fact[row+col-2] / (fact[row-1] * fact[col-1]) ) % MOD ;
+			long up = fact[row+col-2];
+			long down = (fact[row-1] * fact[col-1]) % MOD;
+			long div = (fact[row-1] * fact[col-1]) / MOD;
 
-			List<Point> list = new ArrayList<>();
+			long totalCount = (up + MOD * div) / down;			
+
+
+			System.out.println("First : " + up + " , " + down + " " + totalCount);
+
+			List<Point2> list = new ArrayList<>();
 
 			for(int i=0 ; i<block ; i++) {
 				int x = sc.nextInt();
 				int y = sc.nextInt();
 
 				if(x>=1 && y>=1 && x <= row && y<= col)
-					list.add(new Point(x-1, y-1));
+					list.add(new Point2(x-1, y-1));
 				else { //장애물 체스판 바깥에 존재
 					continue;
 				}
 			}
 
 			Collections.sort(list);
-			
-			long sum = getVal(list.get(0).x, list.get(0).y, (row-1), (col-1));
-			
-			for(int i = 1 ; i < list.size() ; i++) {
-				for(int j = 0 ; j < i ; j++) {
-					// 0 부터 i 번째까지 중복이 있나 체크
-					if(list.get(i).x >= list.get(j).x 
-							&& list.get(i).y >= list.get(j).y) {
-						//중복되므로 제거 필요
-					} else {
-						//중복이 되지 않으므로 따로 계산
-					}
-				}
-			}
-			
-			for(Point c : list) {
-				System.out.println(c.x + " , " + c.y);
-			}
-
-			Answer = totalCount - 0;
 
 			System.out.println("Case #"+(test_case+1));
 			System.out.println(Answer % MOD);
 		}
 		sc.close();
 	}
-	
+
 	public static long getVal(int x, int y, int xSize, int ySize) {
-		long distOne = fact[x+y] / (fact[x]*fact[y]);
-		long distTwo = fact[xSize+ySize-(x+y)] / (fact[xSize-x]*fact[ySize-y]);
-		
+		long distOne = (fact[x+y]) / ( (fact[x]*fact[y]) % MOD ) ;
+		long distTwo = (fact[xSize+ySize-(x+y)]) / ( (fact[xSize-x]*fact[ySize-y]) % MOD ) ;
+
+		//System.out.println("getVal : " + fact[x+y] + " , " + fact[x] + " , " + fact[y]);
+		//System.out.println("getVal : " + fact[xSize+ySize-(x+y)] + " , " + (fact[xSize-x]*fact[ySize-y]));
+		System.out.println("getVal : " + distOne + " , " + distTwo + " , " + ( (distOne * distTwo) % MOD ) );
+
 		return distOne * distTwo;
+	}
+
+	public static long getVal2(int x, int y) {
+		long distOne = (fact[x+y] % MOD) / ( ( ( (fact[x] % MOD) * (fact[y] % MOD) ) % MOD ) );
+
+		//System.out.println("getVal 2 : " + fact[x+y] + " , " + (fact[x] % MOD) * (fact[y] % MOD));
+
+		return distOne;
 	}
 }
 
-class Point implements Comparable<Point>{
+class Point2 implements Comparable<Point2>{
 	int x;
 	int y;
-	
-	public Point(int x, int y){
+
+	public Point2(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	@Override
-	public int compareTo(Point o) {
+	public int compareTo(Point2 o) {
 		if(this.x > o.x){
 			return 1;
 		}else if(this.x == o.x && this.y > o.y){
@@ -95,5 +94,6 @@ class Point implements Comparable<Point>{
 		}
 		return -1;
 	}
-	
+
 }
+
--- Algorithm/src/codeground/Test.java
+++ Algorithm/src/codeground/Test.java
@@ -1,52 +1,134 @@
 package codeground;
-import java.math.BigDecimal;
-import java.math.BigInteger;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Test {
+	static int Answer;
+	static int MOD = 1_000_000_007;
+
 	public static void main(String args[]) throws Exception	{
 		Scanner sc = new Scanner(System.in);
 
-		int T;
-		int test_case;
+		int T = sc.nextInt();
 
-		T = sc.nextInt();        
-		for(test_case = 1; test_case <= T; test_case++) {
-			// 이 부분에서 알고리즘 프로그램을 작성하십시오. 기본 제공된 코드를 수정 또는 삭제하고 본인이 코드를 사용하셔도 됩니다.
-        	 int k = sc.nextInt();
-            BigInteger max = new BigInteger("1");
-            
-            for(int i=0; i<k; i++){
-            	max = max.multiply(BigInteger.valueOf(2));
-            }
-            int total = 0;
-			long min = 1;
-			long num = 0;
-			
-			for(long i=1; ; i++){
-				num = i;
-				
-				for(int j=0;j<k ;j++){
-					if(num%2==0){
-						num = num/2; 
-						total++;
-					}else if(num%2!=0 && num!=1){
-						num = 3*num+1;
-						total++;
-					}else if(num==1){
-						break;
-					}
+		for(int test_case = 0; test_case < T; test_case++) {
+			Answer = 0;
+
+			int row = sc.nextInt();
+			int col = sc.nextInt();
+			int block = sc.nextInt();
+
+			long [] road = new long[row];
+
+			Arrays.fill(road, 1);
+
+			List<Point> list = new ArrayList<>();
+
+			for(int i=0 ; i<block ; i++) {
+				int x = sc.nextInt();
+				int y = sc.nextInt();
+
+				if(x>=1 && y>=1 && x <= row && y<= col)
+					list.add(new Point(x-1, y-1));
+				else { //장애물 체스판 바깥에 존재
+					continue;
 				}
-				
-				if(total==k && num==1 ){
-					min = i;
-					break;
-				}
-				total=0;
 			}
 
-			// 이 부분에서 정답을 출력하십시오.
-			System.out.println("Case #" + test_case);
-			System.out.println(min+" "+max);
+			for(int j = 0 ; j < col ; j++) {
+				for(int i = 0 ; i < row ; i++) {
+					//System.out.print("i : " + i + " , j : " + j + " ");
+
+					if(j == 0) {
+						if(list.contains(new Point(i, j))) {
+							road[i] = -1;
+						} else {
+							if(i >= 1 && road[i-1] == -1) {
+								road[i] = -1;
+							} else {
+								road[i] = 1;
+							}
+						}
+					} else {
+						if(i == 0) {
+							if(list.contains(new Point(i, j))) {
+								//-1
+								road[i] = -1;
+							} else {
+								if(road[i] == -1) {
+									road[i] = -1;
+								} else {
+									road[i] = 1;
+								}
+							}
+						} else {
+							// i >= 1 && j >= 1 일 경우
+							if(list.contains(new Point(i,j))) {
+								road[i] = -1;
+							} else {
+								/*
+								 * 1. 위쪽 + 왼쪽 
+								 * 2. 위쪽 
+								 * 3. 왼쪽
+								 */
+								if(road[i] != -1 && road[i-1] != -1) {
+									long temp = road[i];
+									temp += road[i-1];
+									road[i] = (temp % MOD);
+								} else if(road[i-1] == -1 && road[i] != -1) {
+									road[i] = road[i];
+								} else if(road[i-1] != -1 && road[i] == -1) {
+									road[i] = road[i-1];
+								} else {
+									road[i] = -1;
+								}
+							}
+						}
+					}
+				}
+			}
+
+			System.out.println("Case #"+(test_case+1));
+			System.out.println(road[row-1] % MOD);
 		}
-	}}
+	}
+}
+
+class Point implements Comparable<Point>{
+	int x;
+	int y;
+
+	public Point(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public int compareTo(Point o) {
+		if(this.x > o.x){
+			return 1;
+		}else if(this.x == o.x && this.y > o.y){
+			return 1;
+		}
+		return -1;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		if (this == obj) return true;
+		// 2. null 인지 체크 (모든 객체는 null과 동치 관계가 있지 않다.)
+		if (obj == null) return false;
+		// 3. 인자의 자료형이 정확한지 검사.
+		if (!(obj instanceof Point)) return false;
+		// 4. 자료형 변환
+		Point p = (Point) obj;
+
+		// 5. 중요 필드 점검
+		return this.x == p.x && this.y == p.y;
+	}
+}
+
