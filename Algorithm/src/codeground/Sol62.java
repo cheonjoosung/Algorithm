d43b71252d0a88fa382f5bd53a659ed1d00a63d2
cjswntjd <cheonjoosung@cheonjuseong-ui-MacBook-Pro.local>
2017-09-21 11:45:30 +0900

    good

--- Algorithm/src/codeground/Sol62.java
+++ Algorithm/src/codeground/Sol62.java
@@ -11,64 +11,49 @@ public class Sol62 {
 
 		int T = sc.nextInt();
 		for(int test_case = 0; test_case < T; test_case++) {
-			Answer = "NO";
-
 			int n = sc.nextInt();
 			String s = sc.next();
-
-			if(s.charAt(n-1) == 'A' || s.charAt(n-1) == 'B') {
-				if(n < 3 ) {
-					Answer = "YES";
-				} else if(n >= 3) {
-					String first = s.charAt(n-1)+"";
-					String second = s.charAt(n-2)+"";
-					String third = s.charAt(n-3)+"";
-
-					if(first.equals("A") && second.equals("B") && third.equals("B") 
-							|| first.equals("B") && second.equals("A") && third.equals("A")) {
-						Answer = "NO";
-					} else {
-						Answer = "YES";
-					}
-
-					String temp = first;
-					first = second;
-					second = third;
-
-					if(n > 3) {
-						for(int i=n-4 ; i>=0 ; i--) {
-							third = s.charAt(i)+"";
-
-							if(temp.equals("A")){
-								if(first.equals("A") && second.equals("B") && third.equals("B") 
-										|| first.equals("B") && second.equals("A") && third.equals("A")
-										|| first.equals("C") && second.equals("B") && third.equals("B") ) {
-									Answer = "NO";
-									break;
-								} else {
-									Answer = "YES";
-								}
-							} else {
-								if(first.equals("A") && second.equals("A")
-										|| first.equals("B") && second.equals("A") && third.equals("A")
-										|| first.equals("C") && second.equals("A") && third.equals("A") ) {
-									Answer = "NO";
-									break;
-								} else {
-									Answer = "YES";
-								}
-							}
+			s = s.toLowerCase();
+			
+			if((s.charAt(n-1)+"").equals("c")) {
+				System.out.println("Case #"+(test_case+1));
+				System.out.println("NO");
+			} else {
+				System.out.println("Case #"+(test_case+1));
+				System.out.println("YES");
+			}
+			
+			 
 
 
-							first = second;
-							second = third;
+
+
+			/*
+			if((s.charAt(n-1)+"").equals("C")) {
+				System.out.println("Case #"+(test_case+1));
+				System.out.println("NO");
+			} else {
+
+				if((s.charAt(n-1)+"").equals("A")) {
+
+					for(int i=n-2; i>=0 ; i--) {						
+						if((s.charAt(i+1)+"").equals("B") && (s.charAt(i)+"").equals("B")) {
+							Answer = "NO";
+							break;
 						}
 					}
-				} 
-			} 
 
-			System.out.println("Case #"+(test_case+1));
-			System.out.println(Answer);
+				} else {
+
+					for(int i=n-2; i>=0 ; i--) {
+						if((s.charAt(i+1)+"").equals("A") && (s.charAt(i)+"").equals("A")) {
+							Answer = "NO";
+							break;
+						}
+					}*/
+
 		}
+
+
 	}
 }
