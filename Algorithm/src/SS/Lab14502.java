821fb770612d33fb7b35b7113043e9fb66d2344c
천주성 <cjswntjd@cheonjuseong-ui-iMac.local>
2017-10-12 06:04:08 +0900

    solve lab

--- Algorithm/src/SS/Lab14502.java
+++ Algorithm/src/SS/Lab14502.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Lab14502 {
 	public static int [][] map;
 	public static int Ans;
+	public static int Wall;
 	public static int [] rowDir = {1, 0, -1, 0};
 	public static int [] colDir = {0, -1, 0, 1};
 
@@ -17,59 +18,45 @@ public class Lab14502 {
 
 		map = new int[row][col];
 		Ans = 0;
+		Wall = 0;
 
 		int x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0; 
 
 		for(int i=0; i<col ; i++)
-			for(int j=0; j<row ; j++)
+			for(int j=0; j<row ; j++) {
 				map[j][i] = sc.nextInt();
+				if(map[j][i] == 1) Wall++;
+			}
 
-		while(x1 != (row-3) || y1 != (col-1)) {
+		while(true) {
+			if(y1 > col-1) break;
 			if(map[x1][y1] != 0) {
-				if(x1 + 1 == row) {
-					x1 = 0;
-					y1++;
-				} else {
-					x1++;
-				}
+				if(x1+1 == row) y1++;
+				x1 = (x1+1) % row;
 				continue;
-			}
-
-			if(x1 + 1 == row) {
-				x2 = 0;
-				y2 = y1 + 1;;
 			} else {
-				x2 = x1 + 1;
-				y2 = y1;
+				if(x1+1 == row) y2 = y1 + 1;
+				else y2 = y1;
+				x2 = (x1+1) % row;
 			}
 
-			while(x2 != (row-2) || y2 != (col-1)) {
+			while(true) {
+				if(y2 > col-1) break;
 				if(map[x2][y2] != 0) {
-					if(x2 + 1 == row) {
-						x2 = 0;
-						y2++;
-					} else {
-						x2++;
-					}
+					if(x2 + 1 == row ) y2++;
+					x2 = (x2+1) % row;
 					continue;
-				}
-
-				if(x2 + 1 == row) {
-					x3 = 0;
-					y3 = y2 + 1;;
 				} else {
-					x3 = x2 + 1;
-					y3 = y2;
+					if(x2+1 == row) y3 = y2+1;
+					else y3 = y2;
+					x3 = (x2+1) % row;
 				}
 
-				while(x3 != (row-1) || y3 != (col-1)) {	
+				while(true) {
+					if(y3 > col-1) break;
 					if(map[x3][y3] != 0) {
-						if(x3 + 1 == row) {
-							x3 = 0;
-							y3++;
-						} else {
-							x3++;
-						}
+						if(x3 + 1 == row) y3++;
+						x3 = (x3+1) % row;
 						continue;
 					}
 
@@ -80,36 +67,28 @@ public class Lab14502 {
 							tempMap[j][i] = map[j][i];
 					
 					// 0. 벽놓기
-					tempMap[x1][y1] = 1;
-					tempMap[x2][y2] = 1;
-					tempMap[x3][y3] = 1;
+					map[x1][y1] = 1;
+					map[x2][y2] = 1;
+					map[x3][y3] = 1;
 					// 1. 바이러스 퍼트리기
-					virus(tempMap, row, col);
-					// 2. 안전구역 체크 
-					countZerom(tempMap);
+					virus(row, col);
+					
+					map[x1][y1] = 0;
+					map[x2][y2] = 0;
+					map[x3][y3] = 0;
 
-					if(x3 + 1 == row) {
-						x3 = 0;
-						y3++;
-					} else {
-						x3++;
-					}
+					if(x3 + 1 == row) y3++;
+					x3 = (x3+1) % row;
 				}
-
-				if(x2 + 1 == row) {
-					x2 = 0;
-					y2++;
-				} else {
-					x2++;
-				}
+				
+				if(x2 == row-2 && y2 == col-1) break;
+				if(x2 + 1 == row) y2++;
+				x2 = (x2+1) % row;
 			}
-
-			if(x1 + 1 == row) {
-				x1 = 0;
-				y1++;
-			} else {
-				x1++;
-			}
+			
+			if(x1 == row-3 && y1 == col-1) break;
+			if(x1 + 1 == row) y1++;
+			x1 = (x1+1) % row;
 		}
 		
 		System.out.println(Ans);
@@ -117,47 +96,41 @@ public class Lab14502 {
 		sc.close();
 	}
 
-	public static void virus(int [][] tempMap,int row, int col){
+	public static void virus(int row, int col){
 		boolean [][] visited = new boolean[row][col];
 
 		for(boolean [] temp : visited) 
 			Arrays.fill(temp, false);
 
+		int virusCount = 0;
+		
 		for(int i=0 ; i<col ; i++) {
 			for(int j=0; j<row ; j++) {
 				if(map[j][i] == 2  && visited[j][i] == false) {
-					spread(tempMap, j, i, row, col, visited);
+					virusCount += spread(j, i, row, col, visited);
 				}
 			}
 		}
+		
+		Ans = Math.max(Ans, (row*col - (3 + Wall + virusCount)));
 
 		//dfs or 재귀 
 	}
 
-	public static void spread(int [][] tempMap, int x, int y, int row, int col, boolean [][] visited) {
+	public static int spread(int x, int y, int row, int col, boolean [][] visited) {
 		visited[x][y] = true;
 
+		int res = 1;
 		for(int i=0 ; i<4 ; i++) {
 			int nextX = x + rowDir[i];
 			int nextY = y + colDir[i];
 
-			if (nextX < 0 || nextX >= row || nextY < 0 || nextY >= col 
-					|| tempMap[nextX][nextY] == 1) continue;
+			if (nextX < 0 || nextX >= row || nextY < 0 || nextY >= col) continue;
 
-			if (tempMap[nextX][nextY] != 1 && !visited[nextX][nextY]) {
-				tempMap[nextX][nextY] = 2;
-				spread(tempMap, nextX, nextY, row, col, visited);
+			if (map[nextX][nextY] != 1 && !visited[nextX][nextY]) {
+				res += spread(nextX, nextY, row, col, visited);
 			}
 		}
-
-	}
-
-	public static void countZerom(int [][] tempMap) {
-		int count = 0;
-		for(int [] temp : tempMap)
-			for(int val : temp) 
-				if(val == 0) count++;
-
-		Ans = Math.max(Ans, count);
+		return res;
 	}
 }
--- Algorithm/src/SS/Main.java
+++ Algorithm/src/SS/Main.java
@@ -1,130 +1,89 @@
 package SS;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	public static int [][] map;
-	public static int Ans;
-	public static int Wall;
-	public static int [] rowDir = {1, 0, -1, 0};
-	public static int [] colDir = {0, -1, 0, 1};
-
+	static int Ans = 0;
+	static int Len = 4;
+	static int row, col;
+	static int [][] map;
+	static boolean [][] visited;
+	static int [] dR = {1, 0, -1, 0};
+	static int [] dC = {0, -1, 0, 1};
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		int col = sc.nextInt();
-		int row = sc.nextInt();
-
+		
+		col = sc.nextInt();
+		row = sc.nextInt();
+		
 		map = new int[row][col];
+		visited = new boolean[row][col];
 		Ans = 0;
-		Wall = 0;
-
-		int x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0; 
-
+		
 		for(int i=0; i<col ; i++)
-			for(int j=0; j<row ; j++) {
+			for(int j=0 ; j<row ; j++) 
 				map[j][i] = sc.nextInt();
-				if(map[j][i] == 1) Wall++;
+			
+		for(int i=0 ; i<col ; i++) {
+			for(int j=0; j<row ; j++) {
+				dfs(0, j , i, 0);
+				solve(j, i);
 			}
-
-		while(x1 != (row-3) || y1 != (col-1)) {
-			if(map[x1][y1] != 0) {
-				if(x1 +1 == row) y1++;
-				x1 = (x1+1) % row;
-				continue;
-			} else {
-				if(x1+1 == row) y2 = y1 + 1;
-				else y2 = y1;
-				x2 = (x1+1) % row;
-			}
-
-			while(x2 != (row-2) || y2 != (col-1)) {
-				if(map[x2][y2] != 0) {
-					if(x2 + 1 == row ) y2++;
-					x2 = (x2+1) % row;
-					continue;
-				} else {
-					if(x2+1 == row) y3 = y2+1;
-					else y3 = y2;
-					x3 = (x2+1) % row;
-				}
-
-				while(x3 != (row-1) || y3 != (col-1)) {	
-					if(map[x3][y3] != 0) {
-						if(x3 + 1 == row) y3++;
-						x3 = (x3+1) % row;
-						continue;
-					}
-
-					int [][] tempMap = new int[row][col];
-					
-					for(int i=0 ; i<col ; i++) 
-						for(int j=0 ; j<row ; j++)
-							tempMap[j][i] = map[j][i];
-					
-					// 0. 벽놓기
-					tempMap[x1][y1] = 1;
-					tempMap[x2][y2] = 1;
-					tempMap[x3][y3] = 1;
-					// 1. 바이러스 퍼트리기
-					virus(tempMap, row, col);
-					// 2. 안전구역 체크 
-
-					if(x3 + 1 == row) y3++;
-					x3 = (x3+1) % row;
-				}
-				
-				if(x2 + 1 == row) y2++;
-				x2 = (x2+1) % row;
-			}
-
-			if(x1 + 1 == row) y1++;
-			x1 = (x1+1) % row;
 		}
 		
 		System.out.println(Ans);
-
+		
 		sc.close();
 	}
-
-	public static void virus(int [][] tempMap,int row, int col){
-		boolean [][] visited = new boolean[row][col];
-
-		for(boolean [] temp : visited) 
-			Arrays.fill(temp, false);
-
-		int virusCount = 0;
-		
-		for(int i=0 ; i<col ; i++) {
-			for(int j=0; j<row ; j++) {
-				if(map[j][i] == 2  && visited[j][i] == false) {
-					virusCount = spread(tempMap, j, i, row, col, visited);
-				}
-			}
+	
+	public static void dfs(int depth, int x, int y, int sum) {
+		if(depth == Len - 1) {
+			sum += map[x][y];
+			
+			Ans = Math.max(sum, Ans);
+			return;
 		}
 		
-		Ans = Math.max(Ans, (row*col - (3 + Wall + virusCount)));
-
-		//dfs or 재귀 
-	}
-
-	public static int spread(int [][] tempMap, int x, int y, int row, int col, boolean [][] visited) {
 		visited[x][y] = true;
-
-		int res = 1;
-		for(int i=0 ; i<4 ; i++) {
-			int nextX = x + rowDir[i];
-			int nextY = y + colDir[i];
-
-			if (nextX < 0 || nextX >= row || nextY < 0 || nextY >= col 
-					|| tempMap[nextX][nextY] == 1) continue;
-
-			if (tempMap[nextX][nextY] != 1 && !visited[nextX][nextY]) {
-				tempMap[nextX][nextY] = 2;
-				res += spread(tempMap, nextX, nextY, row, col, visited);
+		
+		for(int i=0; i<4; i++) {
+			int nextX = x + dR[i];
+			int nextY = y + dC[i];
+			
+			if(nextX < 0 || nextY < 0 || nextX >= row || nextY >= col) continue;
+			if(visited[x][y]) continue;
+			
+			dfs(depth+1, nextX, nextY, sum+map[x][y]);
+		}
+		
+		visited[x][y] = false;
+	}
+	
+	public static void solve(int x, int y) {		
+		if(y == 0) {
+			if(x == 0 || x == row-1) return;
+			int temp = map[x-1][y] + map[x][y] + map[x+1][y] + map[x][y+1];
+			Ans = Math.max(Ans, temp);
+		} else if(y == col-1) {
+			if(x == 0 || x == row-1) return;
+			int temp = map[x-1][y] + map[x][y] + map[x+1][y] + map[x][y-1];
+			Ans = Math.max(Ans, temp);
+		} else {
+			if(x == 0) {
+				int temp = map[x][y-1] + map[x][y] + map[x][y+1] + map[x+1][y];
+				Ans = Math.max(Ans, temp);
+			} else if(x == row-1) {
+				int temp = map[x][y-1] + map[x][y] + map[x][y+1] + map[x-1][y];
+				Ans = Math.max(Ans, temp);
+			} else {
+				int temp1 = map[x][y-1] + map[x][y] + map[x][y+1] + map[x+1][y];
+				int temp2 = map[x][y-1] + map[x][y] + map[x][y+1] + map[x-1][y];
+				int temp3 = map[x-1][y] + map[x][y] + map[x+1][y] + map[x][y-1];
+				int temp4 = map[x-1][y] + map[x][y] + map[x+1][y] + map[x][y+1];
+				int temp = Math.max(Math.max(temp1, temp2), Math.max(temp3, temp4));
+				Ans = Math.max(Ans, temp);
 			}
 		}
-		return res;
 	}
 }
