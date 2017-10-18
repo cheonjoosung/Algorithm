672a71887f661a416c15804af501bdd2458640a9
cjswntjd <cheonjoosung@cheonjuseong-ui-MacBook-Pro.local>
2017-10-18 17:40:03 +0900

    add dp

---
+++ Algorithm/src/backjun/etc/P9084.java
@@ -0,0 +1,23 @@
+package backjun.etc;
+
+import java.util.Scanner;
+
+public class P9084 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int testCase = sc.nextInt();
+		
+		for(int i=0 ; i<testCase ; i++) {
+			int coinCount = sc.nextInt();
+			int [] coin = new int[coinCount];
+			for(int j=0 ; j<coinCount ; j++) {
+				coin[j] = sc.nextInt();
+			}
+			int val = sc.nextInt();
+			
+			
+		}
+		sc.close();
+	}
+}
