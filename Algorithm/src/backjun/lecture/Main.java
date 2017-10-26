3fef22eb33fcdbdc98d65add0e4c25375d5f0323
천주성 <cjswntjd@cheonjuseong-ui-iMac.local>
2017-10-26 19:53:39 +0900

    ?

--- Algorithm/src/backjun/lecture/Main.java
+++ Algorithm/src/backjun/lecture/Main.java
@@ -3,33 +3,53 @@ package backjun.lecture;
 import java.util.Scanner;
 
 public class Main {
-    public static int mod = 1000000;
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine().trim();
-        int n = s.length();
-        s = " " + s;
-        int[] d = new int[n+1];
-        d[0] = 1;
-        for (int i=1; i<=n; i++) {
-            int x = s.charAt(i) - '0';
-            if (1 <= x && x <= 9) {
-                d[i] += d[i-1];
-                d[i] %= mod;
-            }
-            if (i==1) {
-                continue;
-            }
-            if (s.charAt(i-1) == '0') {
-                continue;
-            }
-            x = (s.charAt(i-1)-'0')*10 + (s.charAt(i)-'0');
-            if (10 <= x && x <= 26) {
-                d[i] += d[i-2];
-                d[i] %= mod;
-            }
-        }
-        System.out.println(d[n]);
-    }
+	static int Ans = 0;
+	static int m [][];
+	static int [] dx = {1, 0, -1, 0};
+	static int [] dy = {0, -1, 0 ,1};
+	static boolean [] visited;
+	static int row;
+	static int col;
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+		col = sc.nextInt();
+		row = sc.nextInt();
+
+		m = new int[row][col];
+		visited = new boolean[26];
+		
+		Ans = 1;
+
+		for(int i=0 ; i<col ; i++) {
+			String s = sc.next();
+			for(int j=0; j<s.length() ; j++) {
+				m[j][i] = s.charAt(j) - 'A';
+			}
+		}
+
+		visited[m[0][0]] = true;
+		dfs(0, 0, 1);
+
+		System.out.println(Ans);
+	}
+
+	public static void dfs(int x, int y, int step) {
+		Ans = Math.max(Ans, step);
+		
+		for(int i=0 ; i<4 ; i++) {
+			int nextRow = x + dx[i];
+			int nextCol = y + dy[i];
+			
+			if(nextRow < 0 || nextCol < 0 || nextRow >= row || nextCol >= col) continue;
+			if(visited[m[nextRow][nextCol]]) continue;
+			
+			visited[m[nextRow][nextCol]] = true;
+			dfs(nextRow, nextCol, step+1);
+			visited[m[nextRow][nextCol]] = false;
+		}
+
+	}
 }
 
