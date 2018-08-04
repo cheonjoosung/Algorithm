97169ec13256c194aac95089a02e0247bf9fa1a3
천주성 <cjswntjd@cheonjuseong-ui-iMac.local>
2018-08-04 23:09:57 +0900

    modify ladder_15684

---
+++ Algorithm/src/SS/sw_expert/d3/P3260.java
@@ -0,0 +1,27 @@
+package SS.sw_expert.d3;
+
+import java.util.Scanner;
+
+public class P3260 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int tc = sc.nextInt();
+		
+		for(int t=1 ; t<=tc ; t++) {
+			String aStr = sc.next();
+			String bStr = sc.next();
+			
+			char [] aChar = aStr.toCharArray();
+			char [] bChar = bStr.toCharArray();
+			
+			if(aChar.length > bChar.length) {
+				
+			}
+			
+			System.out.println("#t " + 0);
+		}
+		
+		sc.close();
+	}
+}
