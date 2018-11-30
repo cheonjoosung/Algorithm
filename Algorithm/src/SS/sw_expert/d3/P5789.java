ba7ae4bdbf6794638087bf4b9d1997778072242a
천주성 <cjswntjd@cheonjuseong-ui-iMac.local>
2018-11-30 18:41:54 +0900

    solve swexpert P5787

---
+++ Algorithm/src/SS/sw_expert/d3/P5789.java
@@ -0,0 +1,33 @@
+package SS.sw_expert.d3;
+
+import java.util.Scanner;
+
+//5789번 현주의 상자 바꾸
+public class P5789 {	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int tc = sc.nextInt();
+
+		for(int t=1 ; t<=tc ; t++) {
+			int n = sc.nextInt();
+			int q = sc.nextInt();
+			
+			int [] arr = new int[n];
+			
+			for(int i=1 ; i<=q ; i++) {
+				int l = sc.nextInt();
+				int r = sc.nextInt();
+				
+				for(int idx=l ; idx<=r ; idx++)
+					arr[idx-1] = i;
+			}
+			System.out.print("#" + t);
+			for(int i=0 ; i<n ; i++)
+				System.out.print(" " + arr[i]);
+			System.out.println();
+		}
+
+		sc.close();
+	}
+}
