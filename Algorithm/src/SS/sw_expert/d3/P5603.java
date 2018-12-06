4f24fa0c3c1b408ca82b3c25c4423ffb478a89d9
천주성 <cjswntjd@cheonjuseong-ui-iMac.local>
2018-12-06 16:03:12 +0900

    solve swexpert P5603

---
+++ Algorithm/src/SS/sw_expert/d3/P5603.java
@@ -0,0 +1,41 @@
+package SS.sw_expert.d3;
+
+import java.util.Scanner;
+
+//5642번 [Professional] 건초더미
+public class P5603 {
+	static int ans, n;
+	static int [] arr;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+
+		for(int tc=1 ; tc<=t ; tc++) {
+			n = sc.nextInt();
+			
+			ans = 0;
+			int sum = 0;
+			int avg = 0;
+			arr = new int[n];
+			
+			for(int i=0 ; i<n ; i++) {
+				arr[i] = sc.nextInt();
+				sum += arr[i];
+			}
+			
+			avg = sum/n;
+			
+			for(int i=0 ; i<n ; i++) {
+				if(arr[i] > avg) {
+					ans += (arr[i] - avg);
+				}
+			}			
+			
+			System.out.println("#" + tc + " " + ans);
+		}
+
+		sc.close();
+	}
+}
